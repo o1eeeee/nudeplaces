7b9aa95cdb2da2724e9adcb20695c73b6d4adc00
Ole <ole@cardleaders.de>
2021-06-24 09:50:36 +0200

    * add nprogress

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,25 @@
+import Router from 'next/router';
 import '../styles/globals.css'
 import '../styles/fonts/nudeplaces/style.css'
+import NProgress from 'nprogress';
+
+NProgress.configure({ showSpinner: true });
+
+Router.onRouteChangeStart = () => {
+  //console.log('onRouteChangeStart triggered');
+  NProgress.start();
+};
+
+Router.onRouteChangeComplete = () => {
+  //console.log('onRouteChangeComplete triggered');
+  NProgress.done();
+};
+
+Router.onRouteChangeError = () => {
+  //console.log('onRouteChangeError triggered');
+  NProgress.done();
+};
+
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
