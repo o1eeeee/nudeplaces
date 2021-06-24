52688a3ab0b7c983e208808b238b046a24817603
Ole <ole@cardleaders.de>
2021-06-25 00:40:18 +0200

    * zoom control bottomright

--- components/Map.js
+++ components/Map.js
@@ -1,5 +1,5 @@
 /** https://stackoverflow.com/questions/57704196/leaflet-with-next-js */
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+import { MapContainer, Marker, Popup, TileLayer, ZoomControl } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css';
 import 'leaflet-defaulticon-compatibility';
@@ -10,11 +10,12 @@ export default function Map() {
     const { mapPosition, markerPositions, zoom } = useMapContext();
 
     return (
-        <MapContainer center={mapPosition} zoom={zoom} scrollWheelZoom={false} style={{ height: "100vh", flex: "1 1 0%" }}>
+        <MapContainer zoomControl={false} center={mapPosition} zoom={zoom} scrollWheelZoom={false} style={{ height: "100vh", flex: "1 1 0%" }}>
             <TileLayer
                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
+            <ZoomControl position="bottomright" />
             {!markerPositions ?
                 <Marker position={mapPosition}></Marker> :
                 markerPositions.map((markerPosition, index) => (
