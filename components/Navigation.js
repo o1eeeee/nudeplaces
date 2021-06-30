1ec797c6136265e1db543d46c864ad331c3c8ae9
Ole <ole@cardleaders.de>
2021-06-30 17:31:08 +0200

    * adjust back button behaviour

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,19 +1,16 @@
-import Link from 'next/link';
 import CountrySelect from './CountrySelect';
 import { getCountries } from '../lib/countries';
 import styles from '../styles/components/Navigation.module.css';
+import { useRouter } from 'next/router';
 
-export default function Navigation({ backButtonHref }) {
+export default function Navigation() {
+    const router = useRouter();
     const countries = getCountries();
     return (
         <div className={styles.navigation}>
-            {backButtonHref && (
-                <Link href={backButtonHref}>
-                    <a className={styles.backButton}>
-                        &larr;
-                    </a>
-                </Link>
-            )}
+            <button type="button" className={styles.backButton} onClick={() => router.back()}>
+                &larr;
+            </button>
             <CountrySelect countries={countries} />
         </div>
     )
