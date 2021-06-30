6d76e3fa8048725070230296b40a3f23e8feecef
Ole <ole@cardleaders.de>
2021-06-30 10:42:33 +0200

    * use env email in ReportLocationButton

--- components/ReportLocationButton.js
+++ components/ReportLocationButton.js
@@ -1,6 +1,7 @@
 import styles from '../styles/components/ReportLocationButton.module.css';
 
-export default function ReportLocationButton({ locationData, email }) {
+export default function ReportLocationButton({ locationData }) {
+    const email = process.env.WEBSITE_EMAIL;
     const subject = `Report Location: ${locationData.title}, ${locationData.country}`;
     const privacyStatement = "By sending this email, you agree that the data you provide is processed in order to solve the issue. All data will be deleted after the issue was solved.";
     const body = `${privacyStatement}%0D%0A%0D%0A${locationData.url}%0D%0A%0D%0APlease specify the issue below:%0D%0A%0D%0A`;
