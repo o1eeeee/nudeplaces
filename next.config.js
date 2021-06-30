3ce436c5fd3858b4b40b955837090d7b9aef54ee
Ole <ole@cardleaders.de>
2021-06-30 10:41:55 +0200

    * add website url and email to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   reactStrictMode: true,
   env: {
+    WEBSITE_URL: process.env.WEBSITE_URL,
+    WEBSITE_EMAIL: process.env.WEBSITE_EMAIL,
     FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
     FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
     FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
