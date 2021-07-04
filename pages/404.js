4e40d92711525b4f005906a359875a96a6f9561c
Ole <ole@cardleaders.de>
2021-07-04 13:20:14 +0200

    * custom error pages

---
+++ pages/404.js
@@ -0,0 +1,13 @@
+import Layout from "../components/Layout"
+
+export default function Custom404() {
+
+    return (
+        <Layout>
+            <h1>
+                404 - Page Not Found
+            </h1>
+            <p>The page you requested does not exist. Please go back to the start page.</p>
+        </Layout>
+    )
+}
---
+++ pages/500.js
@@ -0,0 +1,13 @@
+import Layout from "../components/Layout"
+
+export default function Custom500() {
+
+    return (
+        <Layout>
+            <h1>
+                500 - Internal Server Error
+            </h1>
+            <p>An error occurred when processing your request. Please try again later.</p>
+        </Layout>
+    )
+}
