8fb1647e777cf7cf4d5c8639f29108a483aee955
Ole <ole@cardleaders.de>
2021-07-07 14:43:32 +0200

    * attempt sitemap fix

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,67 +1,62 @@
-import fs from 'fs';
+import { renderToStaticMarkup } from "react-dom/server";
 import { getCountries } from '../lib/countries';
 
-const Sitemap = () => {
+const Sitemap = ({ pages }) => {
 
+  return (
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      {pages?.map((page, index) => {
+        return (
+          <url key={index}>
+            <loc>{`${process.env.WEBSITE_URL}/${page.url}`}</loc>
+            <lastmod>{page?.lastmod}</lastmod>
+          </url>
+        );
+      })}
+    </urlset>
+  )
 }
 
 export default Sitemap;
 
-export const getServerSideProps = ({ res }) => {
-    const baseUrl = process.env.WEBSITE_URL;
-
-    const staticPages = fs
-        .readdirSync("pages")
-        .filter((staticPage) => {
-            return ![
-                "_app.js",
-                "_document.js",
-                "[country]",
-                "404.js",
-                "500.js",
-                "sitemap.xml.js",
-            ].includes(staticPage);
-        })
-        .map((staticPagePath) => {
-            return `${baseUrl}/${staticPagePath}`;
-        });
+export const getServerSideProps = async ({ res }) => {
+  if (res) {
+    const dateNow = new Date().toISOString();
+    const pages = [
+      {
+        url: "",
+        lastmod: dateNow,
+      },
+      {
+        url: "about",
+        lastmod: dateNow,
+      },
+      {
+        url: "add",
+        lastmod: dateNow,
+      },
+    ];
 
     const countries = getCountries();
 
-    const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-      <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-        ${staticPages
-            .map((url) => {
-                return `
-              <url>
-                <loc>${url}</loc>
-                <lastmod>${new Date().toISOString()}</lastmod>
-                <changefreq>monthly</changefreq>
-                <priority>1.0</priority>
-              </url>
-            `;
-            })
-            .join("")}
-        ${countries
-            .map((country) => {
-                return `
-                  <url>
-                    <loc>${baseUrl}/${country.urlName}</loc>
-                    <lastmod>${new Date().toISOString()}</lastmod>
-                    <changefreq>weekly</changefreq>
-                    <priority>1.0</priority>
-                  </url>
-                `;
-            })
-            .join("")}
-      </urlset>
-    `;
+    countries.map((country) => {
+      pages.push({
+        url: country.urlName,
+        lastmod: dateNow,
+      })
+    })
+
 
     res.setHeader("Content-Type", "text/xml");
-    res.write(sitemap);
+    res.write(renderToStaticMarkup(
+      <Sitemap pages={pages} />
+    ));
     res.end();
 
-    return {
-        props: {},
-    };
+  }
+
+  return {
+    props: {},
+  };
+
 };
