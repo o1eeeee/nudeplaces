49f4c128bc8b2eac83f18c2de07308f680d4673d
Ole <ole@cardleaders.de>
2021-07-19 15:23:48 +0200

    * add label text to AddLocationButton

--- components/AddLocationButton.js
+++ components/AddLocationButton.js
@@ -1,10 +1,12 @@
 import Link from 'next/link';
+import { useLanguageContext } from '../context/LanguageProvider';
 import styles from '../styles/components/AddLocationButton.module.css';
 
 export default function AddLocationButton() {
+    const { dictionary } = useLanguageContext();
     return (
         <Link href={'/add'}>
-            <a className={styles.addButton}><span className="icon-location"></span></a>
+            <a className={styles.addButton}><span className="icon-location"></span><span className={styles.addButtonText}>{dictionary("addLocationButtonText")}</span></a>
         </Link>
     )
 }
