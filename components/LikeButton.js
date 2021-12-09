84f6d17786a95ef4b2207adb5b23895bd2af3339
Ole <ole@cardleaders.de>
2021-12-09 16:15:17 +0100

    * improve LikeButton component

--- components/LikeButton.js
+++ components/LikeButton.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useLanguageContext } from '../context/LanguageProvider';
 import { useModalContext } from '../context/ModalProvider';
 import styles from '../styles/components/LikeButton.module.css';
@@ -8,18 +8,23 @@ export default function LikeButton({ location }) {
     const { setIsShown, setModalContent } = useModalContext();
     const { dictionary } = useLanguageContext();
     const [wasClicked, setWasClicked] = useState(false);
+    const [likeCount, setLikeCount] = useState(location.location_reviews.length);
+
+    useEffect(() => {
+        if (wasClicked) {
+            setLikeCount(likeCount + 1)
+        }
+    }, [wasClicked])
 
     const handleClick = () => {
-        setModalContent(<ReviewLocationForm location={location} isSubmitting={false} setIsSubmitting={() => {}} setIsSubmitted={() => {}} />); 
+        setModalContent(<ReviewLocationForm location={location} setIsSubmitted={setWasClicked} />);
         setIsShown(true);
     };
 
     return (
         <button className={wasClicked ? styles.likeButtonClicked : styles.likeButton} onClick={handleClick} >
-            <span className="icon-check-circle"></span>
-            {wasClicked
-                ? "Thank you for your feedback!"
-                : "I like this place"}
+            <span className="icon-heart"></span>
+            {likeCount}
         </button>
     )
 }
